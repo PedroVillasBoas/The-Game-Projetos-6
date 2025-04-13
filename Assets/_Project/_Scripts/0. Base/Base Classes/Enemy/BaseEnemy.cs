375d9fecd439a0374bb811dd127f761cac5f4cdc
Pedro Vilas Bôas <phvb20@gmail.com>
2025-04-13 14:48:29 -0300

    Add: Scene Ref Package

---
+++ Assets/_Project/_Scripts/0. Base/Base Classes/Enemy/BaseEnemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.AI;
+using GoodVillageGames.Game.Core.GameObjectEntity;
+
+namespace GoodVillageGames.Game.Core.Enemy.AI
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class BaseEnemy : Entity
+    {
+        
+    }
+}
